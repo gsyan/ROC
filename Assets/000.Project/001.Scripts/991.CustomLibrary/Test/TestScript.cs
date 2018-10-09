a0def86687d14d8591b6db7a8b9bd9d6d0abd457
gsyan <gsyan@naver.com>
2018-10-10 08:36:16 +0900

    Signed-off-by: gsyan <gsyan@naver.com>

--- Assets/000.Project/001.Scripts/991.CustomLibrary/Test/TestScript.cs
+++ Assets/000.Project/001.Scripts/991.CustomLibrary/Test/TestScript.cs
@@ -26,26 +26,35 @@ public class TestScript : MonoBehaviour
         //_lr1.SetPosition(0, p3.position);
         //_lr1.SetPosition(1, p4.position);
 
-        float[,] mat = { { 2, 1, -1, 8 }, { -3, -1, 2, -11 }, { -2, 1, 2, -3 } };
-        //float[,] mat = { { 0, 0, 1, 1 }, { 3, 0, 2, 8 }, { 0, 2, 3, 9 } };
-        float[] ans = GaussianElimination1(mat);
-
-        Debug.Log(ans);
-
+        //float[,] mat = { { 2, 1, -1, 8 }, { -3, -1, 2, -11 }, { -2, 1, 2, -3 } };
     }
 
     
 	void Update ()
     {
-        Vector3 point1 = new Vector3(5, 1, 3);
-        Vector3 vector1 = new Vector3(1,4,-2);
-        Debug.DrawLine(point1, vector1 * 100, Color.blue);
+        //Vector3 point1 = new Vector3(5, 1, 3);
+        //Vector3 vector1 = new Vector3(1,4,-2);
+        //Debug.DrawLine(point1, vector1 * 100, Color.blue);
 
-        Vector3 point2 = new Vector3(4, 1, 3);
-        Vector3 vector2 = new Vector3(2, 4, -2);
-        Debug.DrawLine(point2, vector2 * 100, Color.green);
+        //Vector3 point2 = new Vector3(4, 1, 3);
+        //Vector3 vector2 = new Vector3(2, 4, -2);
+        //Debug.DrawLine(point2, vector2 * 100, Color.green);
 
-        bool intersection = LineIntersection(point1, vector1, point2, vector2);
+        //Vector3 p0Position = Vector3.zero;
+        //bool intersection = LineIntersection(point1, vector1, point2, vector2, ref p0Position);
+        //p0.position = p0Position;
+
+        _lr1.SetPosition(0, p1.position);
+        _lr1.SetPosition(1, p2.position);
+
+        _lr2.SetPosition(0, p3.position);
+        _lr2.SetPosition(1, p4.position);
+
+        Vector3 p0Position = Vector3.zero;
+        bool intersection = LineIntersection(p1.position, p2.position - p1.position, p3.position, p4.position - p3.position, ref p0Position);
+        p0.position = p0Position;
+
+        
     }
 
 
@@ -71,7 +80,7 @@ public class TestScript : MonoBehaviour
         _lr1.SetPositions(points);
     }
 
-    bool LineIntersection(Vector3 p1, Vector3 v1, Vector3 p2, Vector3 v2)
+    bool LineIntersection(Vector3 p1, Vector3 v1, Vector3 p2, Vector3 v2, ref Vector3 p0)
     {
         //아래의 주석된 내용을 바탕으로 계산
         //p1 = (x1, y1, z1), v1 = (a1, b1, c1) , p2 = (x2, y2, z2), v2 = (a2, b2, c2)
@@ -86,22 +95,50 @@ public class TestScript : MonoBehaviour
         //b1 * t - b2 * s + (y1 - y2) = 0
         //c1 * t - c2 * s + (z1 - z2) = 0
 
+        //직선의 백터가 하나라도 0인 경우는 걸러서 가우스 소거법을 쓴다.
 
-        
+        float A1, B1, C1;
+        float A2, B2, C2;
+        A1 = v1.x;
+        B1 = v2.x;
+        C1 = p2.x - p1.x;
+        A2 = v1.y;
+        B2 = v2.y;
+        C2 = p2.y - p1.y;
+
+        float[,] mat = { { A1, B1, C1 }, { A2, B2, C2 } };
+        float[] ans = GaussianElimination(2, mat);
+
+        p0 = p1 + ans[0] * v1;
 
         return true;
     }
 
-    float[] GaussianElimination1(float[,] mat)
+    bool LineIntersection2(Vector3 p1, Vector3 v1, Vector3 p2, Vector3 v2, ref Vector3 p0)
     {
-        //상삼각행렬을 만드는 작업
-        int row = 3;
-        int column = 4;
+        float det = v1.cross
+
+        return true;
+    }
+
+    float[] GaussianElimination(int row, float[,] mat)
+    {
+        int column = row + 1;
         float[] ans = new float[row];
+        float divisor = 1.0f;
+        //상삼각행렬을 만드는 작업
+        //row 1, col 0 에 기준점을 잡고 시작
+        //row 1, col 0 을 포함해 col 0의 row 값들을 모두 0으로 만드는 작업 수행
+        //col 0 가 끝나면
+        //row 2, col 1 에 기준점을 잡고 다시 시작
+        //row 2, col 1 을 포함해 col 1의 row 값들을 모두 0으로 만드는 작업 수행, 반복
         for (int c = 0; c < column - 1; c++)
         {
             for (int r = c + 1; r < row; r++)//row 는 두번째 줄 부터 시작한다.
             {
+                ReRowOfMat(ref mat, row, column, c, c);
+                if(mat[r, c] == 0.0f) { continue; }//0으로 만들어야 할 놈이 이미 0이라면 continue
+
                 float t = -mat[r, c] / mat[c, c];//곱해질 수
                 float[] l = new float[column];
 
@@ -115,31 +152,53 @@ public class TestScript : MonoBehaviour
                 }
             }
         }
-        for (int i = row - 1; i >= 0; i--)
+        for (int r = row - 1; r >= 0; --r)
         {
-            //ans[i] = a[i][column-1]/a[i][column-2];
-            float t = mat[i, column - 1];
-            for (int j = column - 2; j > i; j--)
+            float t = mat[r, column - 1];
+            for (int c = column - 2; c > r; --c)
             {
 
-                t -= (mat[i,j] * ans[j]);
+                t -= (mat[r, c] * ans[c]);
             }
-            ans[i] = t / mat[i,i];
+            ans[r] = t / mat[r, r];
         }
         return ans;
     }
-
-    float[] GaussianElimination2(float[,] mat)
+    private bool ReRowOfMat(ref float[,] mat, int row, int col, int checkRow, int checkCol)
     {
-        int row = 3;
-        int column = 4;
-        float[] ans = new float[row];
+        //divisor 가 0이 되면 안된다. 0이면 밑에 row 들을 검색해서 0이 아닌 row와 스위칭을 한다.
+        //false 리턴: row 재조정 없이 통과
+        //true 리턴 : row 재조정 된 것
+        float[] zeroRowValues = new float[col];//check point 값이 0인 row의 모든 값들을 임시로 담을 그릇
 
-        
+        if (mat[checkRow, checkCol] == 0.0f)
+        {
+            for(int c = 0; c < col; ++c)
+            {
+                zeroRowValues[c] = mat[checkRow, c];
+            }
 
-
-        return ans;
+            for( int r = checkRow + 1; r < row; ++r)
+            {
+                if (mat[r, checkCol] != 0.0f)
+                {
+                    for (int c = 0; c < col; ++c)
+                    {
+                        //스위칭
+                        mat[checkRow, c] = mat[r, c];
+                        mat[r, c] = zeroRowValues[c];
+                    }
+                    return true;
+                }
+            }
+        }
+        else
+        {
+            return false;
+        }
+        return false;
     }
+
 
     bool LUDecomposition()
     {
